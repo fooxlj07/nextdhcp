7e04fdd61171bf7c3b7a00c24441c0b2f2a372c6
Jing LI <jli@scaleway.com>
2021-06-23 18:49:25 +0200

    t

--- plugin/prometheus/handler.go
+++ plugin/prometheus/handler.go
@@ -21,13 +21,13 @@ func (p *Plugin) ServeDHCP(ctx context.Context, req, res *dhcpv4.DHCPv4) error {
 		extraLabelValues = append(extraLabelValues, label.value)
 	}
 	start := time.Now()
-	defer func(start time.Time) {
-		requestType := req.MessageType().String()
-		responseType := res.MessageType().String()
-		requestDuration.WithLabelValues(append([]string{requestType, responseType}, extraLabelValues...)...).Observe(float64(time.Since(start).Seconds()))
-		requestCount.WithLabelValues(append([]string{requestType}, extraLabelValues...)...).Inc()
-		log.Printf("Prometheus monitoring request_type: %s, response_type: %s", requestType, responseType)
-	}(start)
+	err := p.Next.ServeDHCP(ctx, req, res)
 
-	return p.Next.ServeDHCP(ctx, req, res)
+	requestType := req.MessageType().String()
+	responseType := res.MessageType().String()
+	requestDuration.WithLabelValues(append([]string{requestType, responseType}, extraLabelValues...)...).Observe(float64(time.Since(start).Seconds()))
+	requestCount.WithLabelValues(append([]string{requestType}, extraLabelValues...)...).Inc()
+	log.Printf("Prometheus monitoring request_type: %s, response_type: %s", requestType, responseType)
+
+	return err
 }
